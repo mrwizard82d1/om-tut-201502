3c054ffb2bfc4218ea9cd3d95dcb3f62aa5f61e9
Larry Jones <mrwizard82d1@gmail.com>
2015-02-24 09:19:31 -0600

    Look, Ma! No templates!

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -10,11 +10,19 @@
 
 (defonce app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
 
+;; I believe you must define the function `stripe` before it is used
+;; by `om/root`. (But I'm uncertain. Something is ticking the back of
+;; my brain.)
+
+(defn stripe [text background-color]
+  (let [st #js {:backgroundColor background-color}]
+    (dom/li #js {:style st} text)))
+
 (om/root
   (fn [data owner]
     (om/component (apply dom/ul #js {:className "animals"}
-                         (map (fn [text] (dom/li nil text))
-                              (:list data)))))
+                         (map stripe (:list data)
+                              (cycle ["#ff0" "#fff"])))))
   app-state
   {:target (. js/document (getElementById "app0"))})
 
