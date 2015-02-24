ae05b8b2a62943dfd8a2c79e1a7d4ad216975d17
Larry Jones <mrwizard82d1@gmail.com>
2015-02-24 09:34:47 -0600

    Began creating first component (does not yet work).

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -8,21 +8,42 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+(defonce app-state
+  (atom
+   {:contacts
+    [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+     {:first "Alyssa" :middle-initial "P" :last "Hacker"
+      :email "aphacker@mit.edu"}
+     {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+     {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+     {:first "Cy" :middle-inital "D" :last "Effect"
+      :email "bugs@mit.edu"}
+     {:first "Lem" :middle-initial "E" :last "Tweakit"
+      :email "morebugs@mit.edu"}]}))
 
-;; I believe you must define the function `stripe` before it is used
-;; by `om/root`. (But I'm uncertain. Something is ticking the back of
-;; my brain.)
+(defn contacts-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+               (dom/h2 nil "Contact list")
+               (apply dom/ul nil
+                      (om/build-all contact-view
+                                    (:contacts data)))))))
 
-(defn stripe [text background-color]
-  (let [st #js {:backgroundColor background-color}]
-    (dom/li #js {:style st} text)))
+;; So much for my earlier ticklishness. I **must** define functions
+;; before I use them. :)
+;; When I save the file, figwheel compiles the code, but when it
+;; encounters a warning, it refuses to load the code and so I see
+;; errors. 
+(defn contact-view [contact owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil (display-name contact)))))
 
 (om/root
-  (fn [data owner]
-    (om/component (apply dom/ul #js {:className "animals"}
-                         (map stripe (:list data)
-                              (cycle ["#ff0" "#fff"])))))
+  contact-view
   app-state
   {:target (. js/document (getElementById "app0"))})
 
